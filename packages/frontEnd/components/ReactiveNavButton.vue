16a748d4d5301d38a864261b4f7434ad8a669a41
vvsubash <subash.venkat007@gmail.com>
2019-12-22 07:22:31 +0530

    feat(frontEnd): reactive button to user login
    switches to user authenticaton

--- packages/frontEnd/components/ReactiveNavButton.vue
+++ packages/frontEnd/components/ReactiveNavButton.vue
@@ -1,9 +1,21 @@
 <template>
-  <div><a class="paper-btn" href="#">Github</a></div>
+  <div>
+    <a @click="signin" class="paper-btn">{{
+      this.$store.state.user != null ? this.$store.state.user : 'Login'
+    }}</a>
+  </div>
 </template>
 
 <script>
-export default {}
+// eslint-disable-next-line
+import { mapState } from 'vuex'
+export default {
+  methods: {
+    signin() {
+      this.$store.dispatch('signInWithGoogle')
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
