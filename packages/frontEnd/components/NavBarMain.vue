c6bd8e278570ef4927ad56ce93475961c4fac429
vvsubash <subash.venkat007@gmail.com>
2020-01-06 09:17:45 +0530

    chore: small editing

--- packages/frontEnd/components/NavBarMain.vue
+++ packages/frontEnd/components/NavBarMain.vue
@@ -1,26 +1,29 @@
 <template>
-  <nav class="border fixed split-nav">
-    <div class="nav-brand">
-      <h3><a href="#">Get PaperCSS</a></h3>
-    </div>
-    <div class="collapsible">
-      <input id="collapsible1" type="checkbox" name="collapsible1" />
-      <button>
-        <label for="collapsible1">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
-        </label>
-      </button>
-      <div class="collapsible-body">
-        <ul class="inline">
-          <li><a href="#">Documentation</a></li>
-          <li><a href="#">About</a></li>
-          <li><ReactiveNavButton /></li>
-        </ul>
+  <div class="row">
+    <nav class="split-nav col">
+      <div class="nav-brand">
+        <h3><a href="#">Swatch Milk</a></h3>
       </div>
-    </div>
-  </nav>
+      <div class="collapsible">
+        <input id="collapsible1" type="checkbox" name="collapsible1" />
+        <button>
+          <label for="collapsible1">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </label>
+        </button>
+        <div class="collapsible-body">
+          <ul class="inline">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Price</a></li>
+            <li><ReactiveNavButton /></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -32,4 +35,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+nav {
+  background: none;
+}
+</style>
